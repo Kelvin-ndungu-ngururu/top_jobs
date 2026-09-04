1a2cbd071a6f9c309f817bb4df1283a449d2e122
Kelvin Ndungu <kelvinndungungururu@gmail.com>
2026-09-04 11:41:29 +0300

    Initial commit

---
+++ 1_top_paying_jobs.sql
@@ -0,0 +1,27 @@
+/*
+Qn: What are the top paying data analyst jobs?
+    Identify the top paying data analyst roles availale remotely
+    Top paying - soo we need the salaries filled and not Null
+    Output: jobs, roles, salaries - descending order...
+*/
+
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name AS company_name
+FROM
+    job_postings_fact
+LEFT JOIN
+    company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10;
+
